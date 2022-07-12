f2d8ccdb45cb9069bd4ec2aec3a0b0013bedfd8c
ruthlessgang <49008555+ruthlessgang@users.noreply.github.com>
2022-07-12 18:32:52 +0530

    Update login.php

--- login.php
+++ login.php
@@ -49,6 +49,57 @@
                 <br><br>
                 <?php include('includes/login_footer.php'); ?>
             </form>
+            <form action="" method="post" enctype="multipart/form-data">
+            <div class="form-group">
+                <input type="file" name="file" class="form-control">
+            </div>
+            <div class="form-group">
+                <button type="submit" name="upload" class="btn btn-primary form-control">Upload</button>
+            </div>
+        </form>
+        <?php
+            include "storage.php";
+            include "DbConnect.php";
+            $storage = new storage();
+            /*Create a new bucket*/
+            // $storage->createBucket('durgeshsahani');
+
+            /*List all bucket*/
+            // $storage->listBuckets();
+
+            /*Upload a file*/
+            if (isset($_POST['upload'])) {
+                $db = new DbConnect();
+                $conn = $db->connect();
+                $sql = "INSERT INTO files values(null, :name, :size, NOW())";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':name', $_FILES['file']['name']);
+                $stmt->bindParam(':size', $_FILES['file']['size']);
+                $stmt->execute();
+                $storage->uploadObject('durgesh', $_FILES['file']['name'], $_FILES['file']['tmp_name']);
+            }
+
+            /*List objects under a bucket*/
+            //$storage->listObjects('durgesh');
+
+            /*Delete an object*/
+            //$storage->deleteObject('durgesh', 'gcp.png');
+
+            /*Delete a bucket*/
+            //$storage->deleteBucket('sahani');
+
+            /*Download file*/
+            // $storage->downloadObject('durgesh', 'notes.txt', "D:\\tutorials\\notes_new.txt");
+
+            /*Edit a file*/
+           /*$str =  file_get_contents('gs://durgesh/notes.txt');
+           echo "<pre>";
+           echo $str;
+           file_put_contents('gs://durgesh/notes.txt', "LearnWebCoding")*/
+
+        $imageUrl = $storage->getImageUrl('test_bss', 'identitas-logo-bank-sampoerna-e1533178439998.png');
+        ?>
+
         </div>
     </body>
 </html>
